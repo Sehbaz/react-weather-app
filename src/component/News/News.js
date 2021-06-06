4139500c5a413f1e490fa48d5bf2b4656187b16b
Sehbaz Rafik <sehbazrafik@Sehbazs-MBP.broadband>
2021-06-06 20:52:57 +0100

    bug solution 1

--- src/component/News/News.js
+++ src/component/News/News.js
@@ -21,14 +21,20 @@ const News = () => {
 
   const [items, setItems] = useState([]);
 
+  const [newAPIContainer, setNewAPIContainer] = useState("none");
   useEffect(() => {
     fetch(
-      "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7035a2ce18004b138c9f980fe58a0f21&pageSize=5"
+      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7035a2ce18004b138c9f980fe58a0f21&pageSize=5"
     )
       .then((res) => res.json())
       .then(
         (result) => {
           setItems(result.articles);
+          if (result !== "") {
+            setNewAPIContainer("block");
+          } else {
+            setNewAPIContainer("none");
+          }
         }
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -41,6 +47,7 @@ const News = () => {
         <p className="news-title">Top 5 Tech News</p>
       </div>
       <List className={(classes.root, "newList-container")}>
+        <div style={{ display: { newAPIContainer } }}></div>
         {items.map((item) => (
           <ListItem alignItems="flex-start" key={item.author}>
             <ListItemAvatar>
